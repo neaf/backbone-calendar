9ddc41c2e95dbb3e623b37a1e6768eb55d7d31fc
Tomasz Werbicki <tomasz@werbicki.net>
2012-03-07 12:03:37 +0100

    Consistent indentation.

--- main.js
+++ main.js
@@ -6,19 +6,19 @@ function extractDate(date) {
 }
 
 function earlierDate(date1, date2) {
-    if (date1 < date2) {
-        return date1;
-    } else {
-        return date2;
-    }
+	if (date1 < date2) {
+		return date1;
+	} else {
+		return date2;
+	}
 }
 
 function laterDate(date1, date2) {
-    if (date1 > date2) {
-        return date1;
-    } else {
-        return date2;
-    }
+	if (date1 > date2) {
+		return date1;
+	} else {
+		return date2;
+	}
 }
 
 $(function() {
@@ -37,18 +37,18 @@ $(function() {
 			date = extractDate(date);
 			return this.startDate() <= date && date <= this.endDate();
 		},
-        collidesWith: function(event) {
-            var e1 = this;
-            var e2 = event;
-            return e1.get('startDate') < e2.get('endDate') &&
-                e1.get('endDate') > e2.get('startDate');
-        },
-        sortKey: function() {
-            return [this.get('startDate'), this.length()];
-        },
-        length: function() {
-            return this.get('endDate') - this.get('startDate');
-        }
+		collidesWith: function(event) {
+			var e1 = this;
+			var e2 = event;
+			return e1.get('startDate') < e2.get('endDate') &&
+				e1.get('endDate') > e2.get('startDate');
+		},
+		sortKey: function() {
+			return [this.get('startDate'), this.length()];
+		},
+		length: function() {
+			return this.get('endDate') - this.get('startDate');
+		}
 	});
 
 	window.EventList = Backbone.Collection.extend({
@@ -78,94 +78,94 @@ $(function() {
 		model: Day
 	});
 
-    window.EventView = Backbone.View.extend({
-        initialize: function() {
-            this.indent = 0;
-            this.model.bind('change', this.render, this);
-            this.model.bind('remove', this.remove, this);
-        },
-        tagName: 'li',
-        attributes: {
-            'class': 'event'
-        },
-        template: Handlebars.templates.event,
-        render: function() {
-            var content = this.template(this.model.toJSON());
-            this.$el.html(content);
-            this.position();
-            return this;
-        },
-        remove: function() {
-            this.$el.remove();
-        },
-        position: function() {
-            var hourHeight = (100/24);
-            var date = this.options.date;
-            var start = laterDate(this.model.attributes.startDate, date);
-            var endDate = new Date(date);
-            endDate.setDate(date.getDate() + 1);
-            var end = earlierDate(this.model.attributes.endDate, endDate);
-            this.$el.css({
-                'top': hourHeight * start.getHours() + '%',
-                'height': (end.getTime() - start.getTime()) / 3600000 * hourHeight + '%'
-            });
-        },
-        collidesWith: function(view) {
-            return this.model.collidesWith(view.model);
-        },
-        indentUsing: function(view) {
-            this.indent = view.indent + 1;
-        }
-    });
+	window.EventView = Backbone.View.extend({
+		initialize: function() {
+			this.indent = 0;
+			this.model.bind('change', this.render, this);
+			this.model.bind('remove', this.remove, this);
+		},
+		tagName: 'li',
+		attributes: {
+			'class': 'event'
+		},
+		template: Handlebars.templates.event,
+		render: function() {
+			var content = this.template(this.model.toJSON());
+			this.$el.html(content);
+			this.position();
+			return this;
+		},
+		remove: function() {
+			this.$el.remove();
+		},
+		position: function() {
+			var hourHeight = (100/24);
+			var date = this.options.date;
+			var start = laterDate(this.model.attributes.startDate, date);
+			var endDate = new Date(date);
+			endDate.setDate(date.getDate() + 1);
+			var end = earlierDate(this.model.attributes.endDate, endDate);
+			this.$el.css({
+				'top': hourHeight * start.getHours() + '%',
+				'height': (end.getTime() - start.getTime()) / 3600000 * hourHeight + '%'
+			});
+		},
+		collidesWith: function(view) {
+			return this.model.collidesWith(view.model);
+		},
+		indentUsing: function(view) {
+			this.indent = view.indent + 1;
+		}
+	});
 
 	window.DayView = Backbone.View.extend({
 		tagName: 'li',
 		attributes: {
 			'class': 'day'
 		},
-        initialize: function() {
-            this.eventViews = [];
-        },
+		initialize: function() {
+			this.eventViews = [];
+		},
 		template: Handlebars.templates.day,
 		render: function() {
 			var content = this.template(this.model.viewParams());
 			this.$el.html(content);
-            return this;
-        },
-        addEvents: function() {
-            _.each(this.model.events(), this.addEvent, this);
-        },
-        addEvent: function(event) {
-            var view = new EventView({ model: event, date: this.model.attributes.date });
-            this.eventsContainer().append(view.render().el);
-            this.eventViews.push(view);
-            this.sortEventViews();
-            return view;
-        },
-        eventsContainer: function() {
-            return this.$('.events');
-        },
-        sortEventViews: function() {
-            this.eventViews = _.sortBy(this.eventViews, function(e) {
-                return e.model.sortKey();
-            });
-        },
-        reindentEvents: function() {
-            var maxIndent = 0;
-            var views = this.eventViews;
-            _.each(views, function(current, i) {
-                var remaining = views.slice(i + 1);
-                var colliding = _.filter(remaining, current.collidesWith, current);
-                _.each(colliding, function(other) {
-                    other.indentUsing(current);
-                    maxIndent = Math.max(maxIndent, other.indent);
-                });
-            });
-            _.each(views, function(v) {
-                v.$el.css('left', v.indent * 100 / (maxIndent + 1) + '%');
-                v.$el.css('width', 100 / (maxIndent + 1) + '%');
-            });
-        }
+			return this;
+		},
+		addEvents: function() {
+			_.each(this.model.events(), this.addEvent, this);
+		},
+		addEvent: function(event) {
+			var view = new EventView({ model: event, date: this.model.attributes.date });
+			this.eventsContainer().append(view.render().el);
+			this.eventViews.push(view);
+			this.sortEventViews();
+			return view;
+		},
+		eventsContainer: function() {
+			return this.$('.events');
+		},
+		sortEventViews: function() {
+			this.eventViews = _.sortBy(this.eventViews, function(e) {
+				return e.model.sortKey();
+			});
+		},
+		reindentEvents: function() {
+			var maxIndent = 0;
+			var views = this.eventViews;
+			_.each(views, function(current, i) {
+				var remaining = views.slice(i + 1);
+				var colliding = _.filter(remaining, current.collidesWith, current);
+				_.each(colliding, function(other) {
+					other.indentUsing(current);
+					maxIndent = Math.max(maxIndent, other.indent);
+				});
+			});
+			_.each(views, function(v) {
+				v.$el.css('left', v.indent * 100 / (maxIndent + 1) + '%');
+				v.$el.css('width', 100 / (maxIndent + 1) + '%');
+			});
+		}
 	});
 
 	window.Calendar = Backbone.View.extend({
@@ -174,28 +174,28 @@ $(function() {
 			this.daysContainer = this.$('.days');
 			this.dayViews = [];
 		},
-        addDay: function(day) {
-            var view = new DayView({ model: day });
-            view.render();
-            view.addEvents();
-            this.daysContainer.append(view.el);
-            this.dayViews.push(view);
-        },
-        addEvent: function(event) {
-            var views = this.dayViewsForEvent(event);
-            var eventViews = [];
-            _.each(views, function(view) {
-                var eventView = view.addEvent(event);
-                eventViews.push(eventView);
-                view.reindentEvents();
-            });
-        },
-        dayViewsForEvent: function(event) {
-            return _.filter(this.dayViews, function(view) {
-                return event.happensOn(view.model.attributes.date);
-            });
-        }
-    });
+		addDay: function(day) {
+			var view = new DayView({ model: day });
+			view.render();
+			view.addEvents();
+			this.daysContainer.append(view.el);
+			this.dayViews.push(view);
+		},
+		addEvent: function(event) {
+			var views = this.dayViewsForEvent(event);
+			var eventViews = [];
+			_.each(views, function(view) {
+				var eventView = view.addEvent(event);
+				eventViews.push(eventView);
+				view.reindentEvents();
+			});
+		},
+		dayViewsForEvent: function(event) {
+			return _.filter(this.dayViews, function(view) {
+				return event.happensOn(view.model.attributes.date);
+			});
+		}
+	});
 
 	// Init
 	window.Events = new EventList;
